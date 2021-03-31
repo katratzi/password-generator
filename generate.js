b9787657a7e9655ddd12a06852ed88fcbf392fc6
katratzi <ric_clifford@hotmail.com>
2021-03-31 19:51:46 +0100

    slider value text, get slider at start rather than query each time

--- generate.js
+++ generate.js
@@ -1,12 +1,20 @@
 // for luck
 console.log("hello world");
 
+// now I know my abc's
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// we have different types of passwords.  a constant random string. a-snake-case-type
 let currentGen = "constant";
 
-function randLetter() {
-    var letter = letters[Math.floor(Math.random() * letters.length)];
-    return letter
+// slider and it's value
+var slider = document.getElementById("passwordLength");
+var sliderText = document.getElementById("passwordLengthValue");
+sliderText.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    sliderText.innerHTML = this.value;
 }
 
 // get a new random password
@@ -27,8 +35,7 @@ function regenerate() {
 // regen a constant string, no dashes
 function rengerateConstant() {
     let random = "";
-    let length = document.getElementById('passwordLength').value;
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < slider.value; i++) {
         random += randomChar();
     }
     return random;
@@ -37,32 +44,36 @@ function rengerateConstant() {
 // regen a snake-case-password
 function rengerateSnake() {
     let random = "";
-    let length = document.getElementById('passwordLength').value;
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < slider.value; i++) {
         for (let j = 0; j < 5; j++) {
             random += randomChar();
         }
-        // hyphen seperators
-        if (i !== (length - 1)) {
+        // hyphen seperators except at end
+        if (i !== (slider.value - 1)) {
             random += '-';
         }
     }
-    // remove lost '-';
+
     return random;
 }
 
+// radio button for gen type clicked
 function changeGen(radio) {
 
-    // change in generator type
+    // check if we have a change in generator type (can click same radio twice)
     if (currentGen !== radio.value) {
-        console.log('Old value: ' + currentGen);
-        console.log('New value: ' + radio.value);
         currentGen = radio.value;
+
+        // change slider min/max based on gen type
+        slider.max = currentGen === "constant" ? 42 : 6;
+        sliderText.innerHTML = slider.value;
+
         // regen if we change type
         regenerate();
     }
 }
 
+// helper to get a random alphabet character
 function randomChar() {
     var letter = letters[Math.floor(Math.random() * letters.length)];
     // we'll say a 1 in 3 chance of being uppercase
